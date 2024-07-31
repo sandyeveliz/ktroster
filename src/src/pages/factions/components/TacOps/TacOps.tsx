74b65d6c7038522b9340575b0cb870bfb7c10bca
sandy veliz <sandy.e.veliz@gmail.com>
2024-07-31 15:12:41 -0300

    add tacops

--- src/src/pages/factions/components/TacOps/TacOps.tsx
+++ src/src/pages/factions/components/TacOps/TacOps.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import "./TacOps.scss";
+import { StringDescriptionParsed } from "../StringDescriptionParsed";
 
 export type TacOpsProps = {
   tacops: any;
@@ -11,7 +12,27 @@ const TacOps: React.FC<TacOpsProps> = ({ tacops }) => {
 
   if (!tacops) return <div>No tacops</div>;
 
-  return <div className="tacops">TacOps works!</div>;
+  return (
+    <div className="tacops">
+      <h3>TAC OPS</h3>
+      {tacops.map((tacop: any) => (
+        <div key={tacop.id} className="tacop-card">
+          <div className="tacop-header">
+            <span>{tacop.title}</span>
+          </div>
+
+          <div className="tacop-content">
+            <div className="tacop-subtitle">
+              <span>
+                {tacop.archetype} - Faction Tac Op {tacop.tacopseq}
+              </span>
+            </div>
+            <StringDescriptionParsed description={tacop.description} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TacOps;
